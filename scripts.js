ff76ca2d4262a93fa9610bfa205935a2b069f3db
lvhartwell <31671991+lvhartwell@users.noreply.github.com>
2017-10-05 16:10:58 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var friends = ["Trice", "Tori", "Amber", "Ash", "Titi"];
+
+
+for (var i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+    for( var count = 100; count >= 1; count--) { 
+        console.log(count + " pages of code " + friends[i] + " deletes one now it's all wrong");
+}
+}
+
+
+
+
